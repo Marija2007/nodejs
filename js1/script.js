1b5053a85d61ba455ab1c709c9c12605cb412178
Marija2007 <marijapetkova49@gmail.com>
2026-06-22 12:44:06 +0200

    prvo domasno

---
+++ js1/script.js
@@ -0,0 +1,17 @@
+// 1. Normal function
+function imePrezimeNormal() {
+    console.log("Marija Petkova");
+}
+imePrezimeNormal();
+
+// 2. Function expression
+const imePrezimeFunctionExpression = function() {
+    console.log("Marija Petkova");
+}
+imePrezimeFunctionExpression();
+
+// 3. Arrow function
+const imePrezimeArrow = () => {
+    console.log("Marija Petkova");
+}
+imePrezimeArrow();
