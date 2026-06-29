8d8d5dab6365c5529f6f81f5abfcfbd114e2519e
Marija2007 <marijapetkova49@gmail.com>
2026-06-29 17:23:30 +0200

    domasno

---
+++ js3/script.js
@@ -0,0 +1,19 @@
+// 1. Core module (fs)
+const fs = require("fs");
+// 2. Local module
+const sayHello = require("./hello");
+sayHello();
+
+fs.writeFile("homework.txt", "Zdravo od Node.js!", (err) => {
+  if (err) {
+    console.log("Greska pri zapisuvanje:", err);
+  } else {
+    console.log("Fajlot e uspesno zapisan!");
+  }
+});
+// 3. 
+const chalk = require("chalk");
+
+console.log(chalk.green("Ova e zelen tekst!"));
+console.log(chalk.red.bold("Ova e crven i bold tekst!"));
+
